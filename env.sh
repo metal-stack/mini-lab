0ece5263fb091e31c51444504b98e95294bcb049
Markus Fensterer <markus.fensterer@gmail.com>
2020-07-30 15:20:47 +0200

    fix #23, use cumulus 3.7.13, use ubuntu 20.04, use new image URLs, run a small integration test

---
+++ env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+yq_shell() {
+  docker run --rm -i -v ${PWD}:/workdir mikefarah/yq /bin/sh -c "$@"
+}
+
+METAL_STACK_RELEASE_VERSION=$(yq r group_vars/all/images.yaml 'metal_stack_release_version')
+RELEASE_YAML=$(curl -s https://raw.githubusercontent.com/metal-stack/releases/${METAL_STACK_RELEASE_VERSION}/release.yaml)
+METALCTL_IMAGE_TAG=$(yq_shell "echo \"${RELEASE_YAML}\" | yq r - docker-images.metal-stack.control-plane.metalctl.tag")
+
+cat << EOF > .env
+METALCTL_IMAGE_TAG=${METALCTL_IMAGE_TAG}
+EOF
