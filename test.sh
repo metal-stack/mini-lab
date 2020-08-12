de662894e1cebe324e9f126b62b5cfd62a7138a6
Markus Fensterer <markus.fensterer@gmail.com>
2020-08-12 09:31:10 +0200

    fix cleanup of test artifacts of prvious runs

--- test.sh
+++ test.sh
@@ -7,13 +7,21 @@ export TMPDIR=/var/tmp/
 echo "Cleanup artifacts of previous runs"
 make cleanup
 
+# cleanup does not work 100% on the CI-runner - use virsh commands directly
+for i in metalleaf01 metalleaf02 metalmachine01 metalmachine02; do
+    virsh destroy $i || true;
+    virsh undefine $i || true;
+    virsh vol-delete --pool default "$i-sda.qcow2" || true;
+    virsh vol-delete --pool default "$i.img" || true;
+done
+
 echo "Starting mini-lab"
 make up
 
 echo "Waiting for machines to get to waiting state"
 waiting=$(docker-compose run metalctl machine ls | grep Waiting | wc -l)
 minWaiting=2
-until [ $waiting -ge $minWaiting ]
+until [ "$waiting" -ge $minWaiting ]
 do
     echo "$waiting/$minWaiting machines are waiting"
     sleep 5
@@ -28,7 +36,7 @@ make firewall
 echo "Waiting for machines to get to Phoned Home state"
 phoned=$(docker-compose run metalctl machine ls | grep Phoned | wc -l)
 minPhoned=2
-until [ $phoned -ge $minPhoned ]
+until [ "$phoned" -ge $minPhoned ]
 do
     echo "$phoned/$minPhoned machines have phoned home"
     sleep 5
