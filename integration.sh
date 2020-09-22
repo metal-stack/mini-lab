dca96dc9833b6860cca64e82ee7aea74e320bd14
Gerrit <tirreg91@googlemail.com>
2020-09-22 09:49:27 +0200

    Cleanup.

--- integration.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-ansible-playbook -i inventories/control-plane.yaml obtain_role_requirements.yaml
-ansible-galaxy install --ignore-errors -r requirements.yaml
-
-ansible-playbook -i inventories/control-plane.yaml -i ~/.ansible/roles/metal-ansible-modules/inventory/metal.py integration.yaml
