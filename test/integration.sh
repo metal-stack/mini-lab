2207dcd390369068f9c9fbdd0ac1d23422e1b633
Gerrit <Gerrit91@users.noreply.github.com>
2020-09-25 16:07:33 +0200

    Improvements for simpler integration testing (#39)
    * Probably this repo is not the right place, just a demo of what's possible with Ansible right now.
    
    * Moving things slightly.
    
    * Override can be removed.
    
    * Add ci-prep target in pipeline.
    
    * Cleanup.
    
    * export TMPDIR.
    
    * No Make target approach.
    
    * Revert version.
    
    * Comment out firewall SSH check.
    
    * Attempt fix.
    
    * wait max 5 minutes for machines and installation
    
    * bash noob fix
    
    Co-authored-by: Markus Fensterer <markus.fensterer@gmail.com>

--- test/integration.sh
+++ test/integration.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
-
 set -e
-
-export TMPDIR=/var/tmp/
-
-echo "Cleanup artifacts of previous runs"
-make cleanup
-
-# cleanup does not work 100% on the CI-runner - use virsh commands directly
-for i in metalleaf01 metalleaf02 metalmachine01 metalmachine02; do
-    virsh destroy $i || true;
-    virsh undefine $i || true;
-    virsh vol-delete --pool default "$i-sda.qcow2" || true;
-    virsh vol-delete --pool default "$i.img" || true;
-done
 
 echo "Starting mini-lab"
 make up
@@ -21,11 +7,17 @@ make up
 echo "Waiting for machines to get to waiting state"
 waiting=$(docker-compose run metalctl machine ls | grep Waiting | wc -l)
 minWaiting=2
+declare -i attempts=0
 until [ "$waiting" -ge $minWaiting ]
 do
+    if [ "$attempts" -ge 60 ]; then
+        echo "not enough machines in waiting state - timeout reached"
+        exit 1
+    fi
     echo "$waiting/$minWaiting machines are waiting"
     sleep 5
     waiting=$(docker-compose run metalctl machine ls | grep Waiting | wc -l)
+    attempts=$attempts+1
 done
 echo "$waiting/$minWaiting machines are waiting"
 
@@ -36,23 +28,26 @@ make firewall
 echo "Waiting for machines to get to Phoned Home state"
 phoned=$(docker-compose run metalctl machine ls | grep Phoned | wc -l)
 minPhoned=2
+declare -i attempts=0
 until [ "$phoned" -ge $minPhoned ]
 do
+    if [ "$attempts" -ge 60 ]; then
+        echo "not enough machines phoned home - timeout reached"
+        exit 1
+    fi
     echo "$phoned/$minPhoned machines have phoned home"
     sleep 5
     phoned=$(docker-compose run metalctl machine ls | grep Phoned | wc -l)
+    attempts+=1
 done
 echo "$phoned/$minPhoned machines have phoned home"
 
 sleep 10
 
 echo "Adding route to leaf01"
-sudo ip r d 100.255.254.0/24 || true
-$(make route) || true
+$(make route)
 
 echo "Check if SSH login to firewall works"
 ssh -o StrictHostKeyChecking=no metal@100.255.254.1 -C exit
 
 echo "Successfully started mini-lab"
-sudo ip r d 100.255.254.0/24
-make cleanup
