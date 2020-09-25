2d0c59a73b4d81777b224eaafa663f954b2a658b
Markus Fensterer <markus.fensterer@gmail.com>
2020-09-25 15:16:57 +0200

    wait max 5 minutes for machines and installation

--- test/integration.sh
+++ test/integration.sh
@@ -7,11 +7,17 @@ make up
 echo "Waiting for machines to get to waiting state"
 waiting=$(docker-compose run metalctl machine ls | grep Waiting | wc -l)
 minWaiting=2
+attempts=0
 until [ "$waiting" -ge $minWaiting ]
 do
+    if [ "$attempts" -ge 60 ]; then
+        echo "not enough machines in waiting state - timeout reached"
+        exit 1
+    fi
     echo "$waiting/$minWaiting machines are waiting"
     sleep 5
     waiting=$(docker-compose run metalctl machine ls | grep Waiting | wc -l)
+    attempts+=1
 done
 echo "$waiting/$minWaiting machines are waiting"
 
@@ -22,11 +28,17 @@ make firewall
 echo "Waiting for machines to get to Phoned Home state"
 phoned=$(docker-compose run metalctl machine ls | grep Phoned | wc -l)
 minPhoned=2
+attempts=0
 until [ "$phoned" -ge $minPhoned ]
 do
+    if [ "$attempts" -ge 60 ]; then
+        echo "not enough machines phoned home - timeout reached"
+        exit 1
+    fi
     echo "$phoned/$minPhoned machines have phoned home"
     sleep 5
     phoned=$(docker-compose run metalctl machine ls | grep Phoned | wc -l)
+    attempts+=1
 done
 echo "$phoned/$minPhoned machines have phoned home"
 
