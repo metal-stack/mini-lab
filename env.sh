76ba3c17cfca36caeb1235145765dfd3f31f34fb
abloss <8138505+droid42@users.noreply.github.com>
2021-03-03 14:29:26 +0100

    Closes #55 make fails with error: unknown command "/bin/sh" for "yq" (#56)
    Co-authored-by: Alexander Bloß <alexander.bloss@f-i-ts.de>

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 set -e
 
 yq_shell() {
-  docker run --rm -i -v ${PWD}:/workdir mikefarah/yq /bin/sh -c "$@"
+  docker run --rm -i -v ${PWD}:/workdir mikefarah/yq:3 /bin/sh -c "$@"
 }
 
 METAL_STACK_RELEASE_VERSION=$(yq_shell "yq r inventories/group_vars/all/images.yaml 'metal_stack_release_version'")
