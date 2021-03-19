1372cfa047ff53b092ea546fcca99468a30ace03
Grigoriy Mikhalkin <grigoriymikhalkin@gmail.com>
2021-03-19 11:59:45 +0100

    machine3 ci cleanup

--- test/ci-cleanup.sh
+++ test/ci-cleanup.sh
@@ -6,7 +6,7 @@ echo "Cleanup artifacts of previous runs"
 make cleanup
 
 # cleanup does not work 100% on the CI-runner - use virsh commands directly
-for i in metalleaf01 metalleaf02 metalmachine01 metalmachine02; do \
+for i in metalleaf01 metalleaf02 metalmachine01 metalmachine02 metalmachine03; do \
     virsh destroy $i || true; \
     virsh undefine $i || true; \
     virsh vol-delete --pool default "$i-sda.qcow2" || true; \
