073b0fe434f80db0d0db84bfd187f06a591f1b47
GrigoriyMikhalkin <grigoriymikhalkin@gmail.com>
2021-10-19 23:37:32 +0200

    Update scripts/deactivate_offloading.sh
    Co-authored-by: Gerrit <Gerrit91@users.noreply.github.com>

--- scripts/deactivate_offloading.sh
+++ scripts/deactivate_offloading.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-for i in $(sudo ignite ps | grep leaf | cut -c1-16);
+for docker_container_id in $(docker ps | grep ignite | awk '{ print $1 }');
 do
-    docker_container_id=$(sudo ignite inspect vm $i | jq -r '.status.runtime.id' | cut -c1-12)
     echo "deactivate offloading at veth of leaf switch in docker container ${docker_container_id}"
     docker exec "${docker_container_id}" ethtool --offload vm_eth0 tx off
 done;
