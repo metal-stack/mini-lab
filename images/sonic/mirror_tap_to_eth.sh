3ae7f59a873b2edacec5d0977c1a172b233006ec
Robert Volkmann <20912167+robertvolkmann@users.noreply.github.com>
2024-07-25 09:12:50 +0200

    SONiC 202311 (#179)

--- images/sonic/mirror_tap_to_eth.sh
+++ images/sonic/mirror_tap_to_eth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script is taken from https://netdevops.me/2021/transparently-redirecting-packets/frames-between-interfaces/
+# Script is taken from https://netdevops.me/2021/transparently-redirecting-packetsframes-between-interfaces/
 # Read it for better understanding
 
 set -o errexit
@@ -12,10 +12,11 @@ TAP_IF=$1
 INDEX=${TAP_IF:3:3}
 
 ip link set $TAP_IF up
+ip link set $TAP_IF mtu 65000
 
 # create tc eth<->tap redirect rules
 tc qdisc add dev eth$INDEX ingress
-tc filter add dev eth$INDEX parent ffff: protocol all matchall action mirred egress redirect dev $TAP_IF
+tc filter add dev eth$INDEX parent ffff: protocol all u32 match u8 0 0 action mirred egress redirect dev $TAP_IF
 
 tc qdisc add dev $TAP_IF ingress
-tc filter add dev $TAP_IF parent ffff: protocol all matchall action mirred egress redirect dev eth$INDEX
+tc filter add dev $TAP_IF parent ffff: protocol all u32 match u8 0 0 action mirred egress redirect dev eth$INDEX
