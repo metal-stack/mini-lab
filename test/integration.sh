6460a0d36acbf925d6cb5170fdc9ccdfc63208ed
Robert Volkmann <20912167+robertvolkmann@users.noreply.github.com>
2024-08-02 07:27:40 +0200

    Use a nginx container to test outgoing traffic (#186)

--- test/integration.sh
+++ test/integration.sh
@@ -43,7 +43,7 @@ done
 echo "$phoned/$minPhoned machines have phoned home"
 
 echo "Test connectivity to outside"
-make connect-to-cloudflare
+make connect-to-www
 
 echo "Test connectivity from outside"
 ssh -o StrictHostKeyChecking=no -o "PubkeyAcceptedKeyTypes +ssh-rsa" -i files/ssh/id_rsa metal@203.0.113.130 -C exit
