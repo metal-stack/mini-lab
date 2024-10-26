e607f94b605bb088d31deae8fd88d04b2a9cf946
Robert Volkmann <20912167+robertvolkmann@users.noreply.github.com>
2024-10-26 10:00:37 +0200

    Simplify ssh config and use it everywhere (#185)

--- test/integration.sh
+++ test/integration.sh
@@ -46,6 +46,6 @@ echo "Test connectivity to outside"
 make connect-to-www
 
 echo "Test connectivity from outside"
-ssh -o StrictHostKeyChecking=no -o "PubkeyAcceptedKeyTypes +ssh-rsa" -i files/ssh/id_rsa metal@203.0.113.130 -C exit
+ssh -F files/ssh/config 203.0.113.130 -C exit
 
 echo "Successfully started mini-lab"
