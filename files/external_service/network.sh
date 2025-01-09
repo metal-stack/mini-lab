c9ae490498c19a37404f3c44fd36839e766db9b0
Robert Volkmann <robert.volkmann@x-cellent.com>
2025-01-09 20:37:01 +0100

    Second fixes

--- files/external_service/network.sh
+++ files/external_service/network.sh
@@ -2,4 +2,4 @@
 set -o errexit -o xtrace
 
 ip addr add 203.0.113.10/24 dev mini_lab_ext
-ip route add 203.0.113.0/24 via 192.0.2.2 dev mini_lab_ext
+ip route add 203.0.113.128/25 via 203.0.113.128 dev mini_lab_ext
